6fa2e370b9977de8ebfc9731456483c269d4bd1b
Niharika <niha.malhot1@gmaail.com>
2022-07-20 19:27:22 -0500

    Added Search page and some other changes

--- src/main/java/com/qa/demo/pages/LoginPage.java
+++ src/main/java/com/qa/demo/pages/LoginPage.java
@@ -2,4 +2,15 @@ package com.qa.demo.pages;
 
 public class LoginPage {
 
+	public void Login() {
+		System.out.println("You are on Login Page");
+	}
+	
+	public void Uname() {
+		System.out.println("Enter User name");
+	}
+	
+	public void Password() {
+		System.out.println("Enter password");
+	}
 }
